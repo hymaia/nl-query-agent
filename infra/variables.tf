27c6854f08ffc2b879704af85bc6568a8aef2f98
franck cussac <franck.cussac@gmail.com>
2026-05-19 23:07:56 +0200

    fix model id

--- infra/variables.tf
+++ infra/variables.tf
@@ -49,5 +49,5 @@ variable "athena_output_bucket" {
 variable "bedrock_model_id" {
   description = "Bedrock model ID"
   type        = string
-  default     = "anthropic.claude-haiku-4-5-20251001-v1:0"
+  default     = "eu.anthropic.claude-haiku-4-5-20251001-v1:0"
 }
